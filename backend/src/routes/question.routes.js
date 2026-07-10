d0b4accee730ffadfccd0d6e8b5995648b35885a
Miarintsoa Christian <ainafinaritra17@gmail.com>
2026-07-10 11:10:58 +0300

    question.routes.js
    Removed comments indicating public and admin protected routes.

--- backend/src/routes/question.routes.js
+++ backend/src/routes/question.routes.js
@@ -3,10 +3,8 @@ const questionController = require("../controllers/question.controller");
 const router = express.Router();
 const { authMiddleware, adminMiddleware } = require('../middleware/auth.middleware')
 
-// Routes publiques
 router.patch("/:id/upvote", questionController.upvoteQuestion);
 
-// Routes protégées admin
 router.get("/", authMiddleware, adminMiddleware, questionController.getAllQuestions);
 router.delete("/:id", authMiddleware, adminMiddleware, questionController.deleteQuestion);
 router.post("/:id/answer", authMiddleware, adminMiddleware, questionController.answerQuestion);
