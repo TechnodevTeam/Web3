d18a799f2b0cbf9e18249c721dc42574924d6adf
Miarintsoa Christian <ainafinaritra17@gmail.com>
2026-07-10 11:12:50 +0300

    speaker.routes.js
    Removed comments for public and admin routes.

--- backend/src/routes/speaker.routes.js
+++ backend/src/routes/speaker.routes.js
@@ -3,11 +3,9 @@ const speakerController = require("../controllers/speaker.controller");
 const router = express.Router();
 const { authMiddleware, adminMiddleware } = require('../middleware/auth.middleware')
 
-// Routes publiques
 router.get("/", speakerController.getAllSpeakers);
 router.get("/:id", speakerController.getSpeakerById);
 
-// Routes admin (protéger avec auth plus tard)
 router.post("/", authMiddleware, adminMiddleware, speakerController.createSpeaker);
 router.put("/:id", authMiddleware, adminMiddleware, speakerController.updateSpeaker);
 router.delete("/:id", authMiddleware, adminMiddleware, speakerController.deleteSpeaker);
